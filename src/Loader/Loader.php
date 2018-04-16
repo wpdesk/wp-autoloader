98d66cc3b840b77db314d526a0e12ba8aee62bce
dyszczo <krzysztof.dyszczyk@gmail.com>
2018-04-16 15:06:16 +0200

    alpha version of wp loader manager

---
+++ src/Loader/Loader.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Interface for all loaders that can be manager by manager
+ */
+interface WPDesk_Loader {
+	/**@return int */
+	public function get_load_priority();
+
+	/**@return int */
+	public function get_create_priority();
+
+	/** @return bool */
+	public function notify_can_autoload();
+
+	/** @return void */
+	public function notify_can_create_plugin();
+
+	/** @return bool */
+	public function notify_cannot_autoload();
+
+	/** @return bool */
+	public function notify_all_loaded();
+
+	/** @return void */
+	public function update_manager(WPDesk_Loader_Manager $manager);
+}
